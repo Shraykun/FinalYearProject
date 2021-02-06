1f7bb3ccd8f6f604ee654c1daa9660747817c7c4
Shreyash Sanjay <s.sanjay@se18.qmul.ac.uk>
2021-02-06 16:24:34 +0000

    Uploading files and images (Firebase)
    Changed taking a picture to only uploading so images can be sent to Firebase properly

--- app/src/main/java/com/example/beetrackingapp/CameraActivity.java
+++ app/src/main/java/com/example/beetrackingapp/CameraActivity.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -19,14 +20,19 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.Html;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,10 +42,17 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -47,12 +60,19 @@ import java.util.Locale;
 
 public class CameraActivity extends AppCompatActivity {
     //Initialise variables
-    ImageView imageView;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int CAMERA_PERMISSION_CODE = 1;
-    Button btnlocation, btnSubmit;
-    TextView latitude, longitude, country, local, address;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ImageView imageView;
+    private Button btnlocation, btnSubmit, btnCamera;
+    private TextView latitude, longitude, country, local, address;
+    private ProgressBar progressBar;
+
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    private Uri imageUri;
+    private StorageReference mStorageRef;
 
     //Database id
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://bee-tracking-app-9b4ff-default-rtdb.firebaseio.com/");
@@ -72,9 +92,13 @@ public class CameraActivity extends AppCompatActivity {
         country = findViewById(R.id.country);
         local = findViewById(R.id.local);
         address = findViewById(R.id.address);
+        progressBar = findViewById(R.id.uploadProg);
 
         //Initialise fusedLocation
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(CameraActivity.this);
+
+        //Storage
+        mStorageRef = FirebaseStorage.getInstance("gs://bee-tracking-app-9b4ff.appspot.com").getReference("uploads");
 
         btnlocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,64 +117,76 @@ public class CameraActivity extends AppCompatActivity {
 
         //CAMERA SETTINGS
         //Check if device has a camera
-        if (getApplicationContext().getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA_ANY)) {
-            Button btnCamera = (Button) findViewById(R.id.btnCamera);
-            imageView = (ImageView) findViewById(R.id.imageView);
+//        if (getApplicationContext().getPackageManager().hasSystemFeature(
+//                PackageManager.FEATURE_CAMERA_ANY)) {
+//            Button btnCamera = (Button) findViewById(R.id.btnCamera);
+//            imageView = (ImageView) findViewById(R.id.imageView);
+//
+//            //When clicking on button
+//            btnCamera.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                        try {
+//                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//                        } catch (ActivityNotFoundException e) {
+//                            // display error state to the user
+//                        }
+//                    } else {
+//                        requestCameraPermission();
+//                    }
+//
+//                }
+//            });
+//        } else {
+//            Toast.makeText(CameraActivity.this, "There is no Camera feature available", Toast.LENGTH_SHORT).show();
+//        }
 
-            //When clicking on button
-            btnCamera.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        try {
-                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                        } catch (ActivityNotFoundException e) {
-                            // display error state to the user
-                        }
-                    } else {
-                        requestCameraPermission();
-                    }
+        //SELECTING A PICTURE
+        btnCamera = findViewById(R.id.btnCamera);
+        imageView = findViewById(R.id.imageView);
 
-                }
-            });
-        } else {
-            Toast.makeText(CameraActivity.this, "There is no Camera feature available", Toast.LENGTH_SHORT).show();
-        }
+        //When clicking the button
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
 
         //FIREBASE
         btnSubmit = findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String lat = latitude.getText().toString();
-                String longi = longitude.getText().toString();
-                String count = country.getText().toString();
-                String loc = local.getText().toString();
-                String add = address.getText().toString();
-
-                HashMap<String , String> infoMap = new HashMap<>();
-
-                infoMap.put("address", add);
-                infoMap.put("country", count);
-                infoMap.put("local", loc);
-                infoMap.put("latitude", lat);
-                infoMap.put("longitude", longi);
-
-                root.setValue(infoMap);
+                uploadInfo();
             }
         });
 
     }
 
     //CAMERA SETTINGS
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
     //When picture is taken
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            imageView.setImageURI(imageUri);
+        }
+//        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+//        imageView.setImageBitmap(bitmap);
+//        imageUri = data.getData();
     }
 
     //Checks if permission is given, if not as for permission.
@@ -301,5 +337,84 @@ public class CameraActivity extends AppCompatActivity {
             //When location service no available
             startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
+    }
+
+    //Storage settings
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    //FIREBASE SETTINGS
+    private void uploadInfo(){
+        String lat = latitude.getText().toString();
+        String longi = longitude.getText().toString();
+        String count = country.getText().toString();
+        String loc = local.getText().toString();
+        String add = address.getText().toString();
+
+
+
+        if (imageUri != null || lat.equals("")){
+            //Image
+            StorageReference fileRef = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+
+            fileRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setProgress(0);
+                                }
+                            }, 500);
+
+                            Toast.makeText(CameraActivity.this, "Upload Successful", Toast.LENGTH_LONG).show();
+                            fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Uri downloadUrl = uri;
+                                    final String url = downloadUrl.toString();
+
+                                    //Uploading
+                                    HashMap<String , String> infoMap = new HashMap<>();
+
+                                    infoMap.put("address", add);
+                                    infoMap.put("country", count);
+                                    infoMap.put("local", loc);
+                                    infoMap.put("latitude", lat);
+                                    infoMap.put("longitude", longi);
+                                    infoMap.put("image", url);
+
+                                    root.setValue(infoMap);
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(CameraActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            progressBar.setProgress((int) progress);
+                        }
+                    });
+
+
+
+
+        } else {
+            Toast.makeText(this, "No picture or location given", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
